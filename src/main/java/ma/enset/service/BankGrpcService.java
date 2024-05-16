44eef96da8000615f3f87f55a7238db9f6294320
ali-ben-hammad <ali.benhammad-etu@etu.univh2c.ma>
2024-05-16 13:26:40 +0100

    implemented server streaming method

--- src/main/java/ma/enset/service/BankGrpcService.java
+++ src/main/java/ma/enset/service/BankGrpcService.java
@@ -3,6 +3,10 @@ package ma.enset.service;
 import io.grpc.stub.StreamObserver;
 import ma.enset.stubs.Bank;
 import ma.enset.stubs.bankServiceGrpc;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class BankGrpcService extends bankServiceGrpc.bankServiceImplBase {
 
     @Override
@@ -23,8 +27,33 @@ public class BankGrpcService extends bankServiceGrpc.bankServiceImplBase {
 
     @Override
     public void getCurrencyStream(Bank.convertCurrencyRequest request, StreamObserver<Bank.convertCurrencyResponse> responseObserver) {
-        super.getCurrencyStream(request, responseObserver);
+        String form = request.getFrom();
+        String to = request.getTo();
+        double amount = request.getAmount();
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            int count = 0;
+                           @Override
+                           public void run() {
+                                 Bank.convertCurrencyResponse response = Bank.convertCurrencyResponse.newBuilder()
+                                        .setAmount(amount)
+                                        .setFrom(form)
+                                        .setTo(to)
+                                        .setResult(amount * Math.random() * 100)
+                                        .build();
+                                 responseObserver.onNext(response);
+                                    count++;
+                                    if(count == 10) {
+                                        responseObserver.onCompleted();
+                                        timer.cancel();
+                                    }
+                           }
+                       }, 1000, 1000);
+
     }
+
+
 
     @Override
     public StreamObserver<Bank.convertCurrencyRequest> performSteam(StreamObserver<Bank.convertCurrencyResponse> responseObserver) {
